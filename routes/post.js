acd50b1144f19c9dab6d83c9b611e3cae0c9cc7c
Sayan Biswas <poorprogrammer-python@users.noreply.github.com>
2021-03-17 16:33:06 +0530

    Authentication

--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,10 @@ const router = express.Router()
 const User = require('../models/User')
 const { ObjectID } = require("mongodb").ObjectID
 const bcrypt = require("bcrypt")
-const { body, validationResult, check } = require("express-validator")
+const { body, validationResult } = require("express-validator")
+const jwt = require('jsonwebtoken')
+const auth = require('../middleware/auth')
+
 
 // Route for registering new users
 router.post(
@@ -12,8 +15,8 @@ router.post(
     //Validation
     body("email").isEmail(),
     body("password").isLength({ min: 8 }),
-    check("firstName").exists(),
-    check("lastName").exists(),
+    body("firstName").isLength({ min: 1 }),
+    body("lastName").isLength({ min: 1 }),
     async (req, res) => {
 
         const errors = validationResult(req);
@@ -21,7 +24,6 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        
 
         user = User({
             firstName: req.body.firstName,
@@ -41,13 +43,33 @@ router.post(
         )
     })
 
+//Route for user login
 router.post(
-    '/login', (req, res) => {
-        
+    '/login',
+    body("email").isEmail(),
+    body("password").isLength({ min: 4 }),
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        user = await User.findOne(
+            { email: req.body.email }
+        )
+        if (!user) return res.status(400).send("Account not found")
+
+        const validPass = bcrypt.compare(req.body.password, user.password)
+        if (!validPass) return res.status(400).send("Invalid Password")
+
+        const token = jwt.sign({_id : user._id},process.env.TOKEN_SECRET)
+
+        res.header('auth-token',token).send(token)
+
     }
 )
 //Route for adding new friends
-router.post('/add-friend', (req, res) => {
+router.post('/add-friend',auth , (req, res) => {
     User.findOneAndUpdate(
         { _id: req.body._id }, {
         $addToSet: {
